9bc0425dd9bfbc2e22d6c7692616361b71b009b4
Shajith <121110865+shajith42@users.noreply.github.com>
2023-01-24 19:23:30 +0530

    Add files via upload

---
+++ math_connecting.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'connectingTowns' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY routes
+#
+
+def connectingTowns(n, routes):
+    r= 1
+    for i in routes:
+        r *= i
+    return r%1234567
+    
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        routes = list(map(int, input().rstrip().split()))
+
+        result = connectingTowns(n, routes)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
